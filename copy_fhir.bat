39fef5d1741e625226e6458d0f1a3211aa97367b
grahamegrieve <grahameg@gmail.com>
2013-12-03 13:37:16 +1100

    Initial Version of Server

---
+++ copy_fhir.bat
@@ -0,0 +1,8 @@
+copy /Y C:\work\org.hl7.fhir\build\implementations\pascal\*.pas Libraries\refplat\*.*
+copy /Y C:\work\org.hl7.fhir\build\implementations\pascal\*.rc Libraries\refplat\*.*
+copy /Y C:\work\org.hl7.fhir\build\implementations\pascal\support\*.pas libraries\support\*.*
+copy /Y C:\work\org.hl7.fhir\build\implementations\pascal\support\*.inc libraries\support\*.*
+copy /Y C:\work\org.hl7.fhir\build\publish\validation.zip exec\*.*
+copy /Y C:\work\org.hl7.fhir\build\tools\schematron\*.xsl exec\*.*
+copy /Y C:\work\org.hl7.fhir\build\implementations\translations.xml Libraries\refplat\FHIRTranslations.xml
+"C:\Program Files (x86)\Embarcadero\RAD Studio\10.0\bin\brcc32.exe" Libraries\refplat\FHIRTranslations.rc
