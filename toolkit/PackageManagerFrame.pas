33d659f586c5e2dcf6282d654185c150369b4442
Grahame Grieve <grahameg@gmail.com>
2018-10-08 16:41:28 +1000

    fixes for loading argonaut package

--- toolkit/PackageManagerFrame.pas
+++ toolkit/PackageManagerFrame.pas
@@ -286,7 +286,7 @@ begin
       try
         fetch.URL := URLPath([url, 'package.tgz']);
         fetch.Fetch;
-        ok := (fetch.ContentType = 'application/x-compressed') or (fetch.ContentType = 'application/octet-stream');
+        ok := (fetch.ContentType = 'application/x-compressed') or (fetch.ContentType = 'application/octet-stream') or (fetch.ContentType = 'application/x-tar');
       except
         on e : exception do
         begin
@@ -299,7 +299,7 @@ begin
         try
           fetch.URL := url;
           fetch.Fetch;
-          ok := (fetch.ContentType = 'application/x-compressed') or (fetch.ContentType = 'application/octet-stream');
+          ok := (fetch.ContentType = 'application/x-compressed') or (fetch.ContentType = 'application/octet-stream') or (fetch.ContentType = 'application/x-tar');
         except
           on e : exception do
           begin
