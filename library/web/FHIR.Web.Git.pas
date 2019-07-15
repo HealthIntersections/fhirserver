28115f1a89db710c7b07c07c34ece7a88c46da74
Grahame Grieve <grahameg@gmail.com>
2019-07-15 20:15:00 +1000

    add missing files

---
+++ library/web/FHIR.Web.Git.pas
@@ -0,0 +1,22 @@
+unit FHIR.Web.Git;
+
+interface
+
+uses
+   SysUtils, Classes,
+   uGitForDelphi,
+   FHIR.Support.Base;
+
+//Type
+//  TFslGitRepository = class (TFslObject)
+//  private
+//    FName : String;
+//  public
+//    constructor create(url : String); overload;
+//    procedure listBrancesInMaster(list : TStringList);
+//    procedure cloneToLocal(dir : String);
+//  end;
+
+implementation
+
+end.
