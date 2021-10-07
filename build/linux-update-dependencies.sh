221fcde31990212ef4f4da7bd324fe0b15f8f6e2
Grahame Grieve <grahameg@gmail.com>
2021-10-08 10:48:50 +1100

    fix build files

---
+++ build/linux-update-dependencies.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+echo "Updating dependencies for linux"
+
+# ---- initial set up ----------
+
+BUILDDIR=${1:-"/tmp/fsbuild"}
+mkdir -p $BUILDDIR
+pushd $BUILDDIR
+
+cd source
+
+if [ ! -d "PascalTZ" ] ; then
+    git clone https://github.com/dezlov/PascalTZ
+else
+    cd PascalTZ && git pull && cd ..
+fi
+
+if [ ! -d "delphi-markdown" ] ; then
+    git clone https://github.com/grahamegrieve/delphi-markdown
+else
+    cd delphi-markdown && git pull && cd ..
+fi
+
+if [ ! -d "extrasyn" ] ; then
+    git clone https://github.com/mriscoc/extrasyn
+else
+    cd extrasyn && git pull && cd ..
+fi
+
+if [ ! -d "HtmlViewer" ] ; then
+    git clone https://github.com/grahamegrieve/HtmlViewer
+else
+    cd HtmlViewer  && git pull && cd ..
+fi
+
+if [ ! -d "lazarus-ide-tester" ] ; then
+    git clone https://github.com/grahamegrieve/lazarus-ide-tester
+else
+    cd lazarus-ide-tester && git pull && cd ..
+fi
+
+if [ ! -d "QRCodeGenLib4Pascal" ] ; then
+    git clone https://github.com/Xor-el/QRCodeGenLib4Pascal
+else
+    cd QRCodeGenLib4Pascal  && git pull && cd ..
+fi
+
+cd ..
+
+# ---- register the source with lazarus ----------
+
+tools/lazarus/lazbuild source/PascalTZ/package/pascaltz.lpk -q
+tools/lazarus/lazbuild source/extrasyn/extrahighlighters.lpk -q
+tools/lazarus/lazbuild source/extrasyn/extrahighlighters_dsgn.lpk -q
+tools/lazarus/lazbuild source/lazarus-ide-tester/package/idetester.lpk -q
+tools/lazarus/lazbuild source/lazarus-ide-tester/ide/idetester_dsgn.lpk -q
+tools/lazarus/lazbuild source/HtmlViewer/package/FrameViewer09.lpk -q
+tools/lazarus/lazbuild source/QRCodeGenLib4Pascal/QRCodeGenLib/src/Packages/FPC/QRCodeGenLib4PascalPackage.lpk -q
+tools/lazarus/lazbuild source/delphi-markdown/packages/markdownengine.lpk -q
+tools/lazarus/lazbuild source/delphi-markdown/tests/markdowntests.lpk -q
+
+# ----  back to the server ----------
+
+popd
