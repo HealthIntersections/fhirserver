df27e3a4b9235dddedc556555195c6cb392800c6
Grahame Grieve <grahameg@gmail.com>
2021-11-19 19:29:05 +1100

    more osx build work

--- dependencies/Indy10/OpenSSL/IdOpenSSLConsts.pas
+++ dependencies/Indy10/OpenSSL/IdOpenSSLConsts.pas
@@ -8,8 +8,11 @@ const
   CLibCryptoRaw = 'libcrypto';
   CLibSSLRaw = 'libssl';
 
+  {$IFDEF OSX}
+  SSLDLLVers: array [0..0] of string = ('.1.1'); // can't try the unversioned ones on OSX
+  {$ELSE}
   SSLDLLVers: array [0..1] of string = ('', '.1.1');
-
+  {$ENDIF}
   CLibCrypto =
     {$IFDEF CPU32}CLibCryptoRaw + '-1_1.dll'{$ENDIF}
     {$IFDEF CPU64}CLibCryptoRaw + '-1_1-x64.dll'{$ENDIF}
