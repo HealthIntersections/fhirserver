17621b1860adf89e17aefe0b53b62dcf010a7d34
Grahame Grieve <grahameg@gmail.com>
2022-01-28 13:30:31 +1100

    more filling it out

--- library/fhir/fhir_package_upload.pas
+++ library/fhir/fhir_package_upload.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils,
-  fsl_base,
+  fsl_base, fsl_logging, fsl_lang, fsl_npm_client, fsl_npm_cache,
   fhir_client, fhir_factory;
 
 type
@@ -15,8 +15,19 @@ type
 
   TFHIRPackageUploader = class (TFslObject)
   private
+    FAPIResources: boolean;
+    FBulkImport: boolean;
     FClient: TFHIRClientV;
+    FDefinitionResources: boolean;
+    FDependencies: boolean;
+    FExampleResources: boolean;
     FFactory: TFHIRFactory;
+    FNoErrorIfExists: boolean;
+    FOnLog: TLogEvent;
+    FProfileResources: boolean;
+    FQuestionnaireResources: boolean;
+    FStopForErrors: boolean;
+    FTerminologyResources: boolean;
     procedure SetClient(AValue: TFHIRClientV);
     procedure SetFactory(AValue: TFHIRFactory);
   public
@@ -26,6 +37,20 @@ type
 
     property client : TFHIRClientV read FClient write SetClient;
     property factory : TFHIRFactory read FFactory write SetFactory;
+
+    // settings:
+    property TerminologyResources : boolean read FTerminologyResources write FTerminologyResources;
+    property ProfileResources : boolean read FProfileResources write FProfileResources;
+    property APIResources : boolean read FAPIResources write FAPIResources;
+    property QuestionnaireResources : boolean read FQuestionnaireResources write FQuestionnaireResources;
+    property DefinitionResources : boolean read FDefinitionResources write FDefinitionResources;
+    property ExampleResources : boolean read FExampleResources write FExampleResources;
+    property Dependencies : boolean read FDependencies write FDependencies;
+    property BulkImport : boolean read FBulkImport write FBulkImport;
+    property StopForErrors : boolean read FStopForErrors write FStopForErrors;
+    property NoErrorIfExists : boolean read FNoErrorIfExists write FNoErrorIfExists;
+
+    Property OnLog : TLogEvent read FOnLog write FOnLog;
   end;
 
 implementation
@@ -53,7 +78,7 @@ end;
 
 procedure TFHIRPackageUploader.uploadPackage(packageId, version: String);
 begin
-  // todo
+  raise EFSLException.create('Still to be done');
 end;
 
 end.
