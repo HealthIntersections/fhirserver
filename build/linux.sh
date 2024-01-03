6d7a18a97bd3801d54dc504c29036b524a20512f
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2024-01-03 20:06:30 +0100

    work on linux build

--- build/linux.sh
+++ build/linux.sh
@@ -4,16 +4,16 @@
 
 BASEDIR=$(dirname "$0")
 BUILDDIR=${1:-"/tmp/fsbuild"}
-OPENSSL_DIR="$BUILDDIR/openssl"
+OPENSSL_DIR=${2:-"/tmp/openssl"}
 export OPENSSL_DIR
 
 mkdir -p $OPENSSL_DIR
 sudo mkdir /7Zip
 sudo chmod 777 /7Zip
 
-export PATH=$OPENSSL_DIR/bin:$PATH
-export LD_LIBRARY_PATH=$OPENSSL_DIR:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$OPENSSL_DIR/pkgconfig:$PKG_CONFIG_PATH
+# export PATH=$OPENSSL_DIR/bin:$PATH
+# export LD_LIBRARY_PATH=$OPENSSL_DIR:$LD_LIBRARY_PATH
+# export PKG_CONFIG_PATH=$OPENSSL_DIR/pkgconfig:$PKG_CONFIG_PATH
 
 $BASEDIR/linux-dependencies.sh $BUILDDIR;
 $BASEDIR/linux-toolchain.sh $BUILDDIR;
