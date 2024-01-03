428df711d034f4c667be8de413fb8b33308e0446
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2024-01-03 20:15:54 +0100

    only server

--- build/linux.sh
+++ build/linux.sh
@@ -15,7 +15,7 @@ sudo chmod 777 /7Zip
 # export LD_LIBRARY_PATH=$OPENSSL_DIR:$LD_LIBRARY_PATH
 # export PKG_CONFIG_PATH=$OPENSSL_DIR/pkgconfig:$PKG_CONFIG_PATH
 
-$BASEDIR/linux-dependencies.sh $BUILDDIR;
-$BASEDIR/linux-toolchain.sh $BUILDDIR;
-$BASEDIR/linux-libraries.sh $BUILDDIR;
+# $BASEDIR/linux-dependencies.sh $BUILDDIR;
+# $BASEDIR/linux-toolchain.sh $BUILDDIR;
+# $BASEDIR/linux-libraries.sh $BUILDDIR;
 $BASEDIR/linux-fhirserver.sh $BUILDDIR;
